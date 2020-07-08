0fb96aaa399f30422d27f5ba1f114478395c72d0
root <root@09bf14ae66a2>
2020-07-08 01:14:53 +0000

    UM MESsaGe :)>><<??≥…≥æ≥æ…≥æπ≥π“¬øµ∫˙¥©√†çƒ∂ßΩƒ≈ç†ƒ√¥©∫˙∆

---
+++ src/_13_bananas_4eva/bananas_4eva/BANNAS4EVA.java
@@ -0,0 +1,10 @@
+package _13_bananas_4eva.bananas_4eva;
+
+public class BANNAS4EVA {
+public static void main(String[] args) {
+	for (int i = 0; i < 1000; i++) {
+		System.out.println("NOLLIE 360 INWARD HEELFLIP");
+		
+	}
+}
+}
